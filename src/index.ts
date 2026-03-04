bcb4612917652d1597668c95d9e989bf333339b8
Sergio Esteban <sergio.calvo@udea.edu.co>
2026-03-03 20:07:37 -0500

    Añadir importación de estilos CSS y exportación del componente IconNerux

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import "./index.css"
+
+export { IconNerux } from "./components/icons/icon";
 export { InputNerux } from "./components/input/input";
 export { TextNerux } from "./components/texto/texto";
 export { ButtonNerux } from "./components/button/button";
