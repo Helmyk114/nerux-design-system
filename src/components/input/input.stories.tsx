44818d7f4e3580a2d2a3f94286eb27b5ec696e8c
Sergio Esteban <sergio.calvo@udea.edu.co>
2026-03-26 20:08:37 -0500

    Personalización del componente input y del thema HeroUi

--- src/components/input/input.stories.tsx
+++ src/components/input/input.stories.tsx
@@ -13,7 +13,7 @@ type Story = StoryObj<typeof InputNerux>;
 export const Default: Story = {
   render: () => (
     <div className="grid grid-cols-3 gap-6">
-      <InputNerux name="Email" placeholder="Ingrese el correo electronico"></InputNerux>
+      <InputNerux name="Email" placeholder="Ingrese el correo electronico" required></InputNerux>
       <InputNerux name="Email" placeholder="Ingrese el correo electronico" error="Correo incorrecto" required></InputNerux>
       <InputNerux name="Password" placeholder="Ingrese el correo electronico" type="password"></InputNerux>
       <InputNerux name="StarIcon" placeholder="Ingrese el correo electronico" startIcon="IconCircle"></InputNerux>
--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -4,12 +4,12 @@ import {
   FieldError,
   InputGroup,
   Button,
+  cn,
 } from "@heroui/react";
 import type { InputProps } from "./input.type";
 import { IconNerux } from "../icons/icon";
 import { TextNerux } from "../texto";
 import { useState } from "react";
-import clsx from "clsx";
 
 export function InputNerux({
   type = "text",
@@ -25,57 +25,68 @@ export function InputNerux({
     type === "password" ? (isVisible ? "text" : "password") : type;
 
   return (
-    <TextField
-      type={inputType}
-      isInvalid={hasError}
-      name={props.name}
-      isRequired={props.required}
-      isDisabled={props.disabled}
-    >
-      <Label>{props.name}</Label>
-      <InputGroup>
-        <InputGroup.Prefix>
-          {startIcon && <IconNerux name={startIcon} size="sm" />}
-        </InputGroup.Prefix>
-        <InputGroup.Input
-          placeholder={props.placeholder}
-          className={clsx(
-            "text-(--color-primary-text-default)!",
-            "focus-within:border-(--color-primary-bg-default)!",
-            "hover:border-(--color-primary-bg-default)!",
-            "border-(--color-border-default)!",
-            hasError ? "border-(--color-error)! text-(--color-error)!" : "",
+    <div className="nerux-focus--brand">
+      <TextField
+        type={inputType}
+        isInvalid={hasError}
+        name={props.name}
+        isRequired={props.required}
+        isDisabled={props.disabled}
+      >
+        <Label>{props.name}</Label>
+        <InputGroup
+          className={cn(
+            "border-[0.2px] rounded-lg",
+            "border-(--color-border-default)",
+            "focus-within:border-(--color-primary-bg-default)",
+            "hover:border-(--color-primary-bg-default)",
+            hasError
+              ? "border-(--color-error) hover:border-(--color-error)"
+              : "",
           )}
-        />
-        <InputGroup.Suffix>
-          {type === "password" ? (
-            <Button
-              isIconOnly
-              variant="ghost"
-              size="md"
-              aria-label={isVisible ? "Hide password" : "Show password"}
-              onPress={() => setIsVisible(!isVisible)}
-            >
-              {isVisible ? (
-                <IconNerux name="IconEyeClosed" />
-              ) : (
-                <IconNerux name="IconEye" />
-              )}
-            </Button>
-          ) : (
-            endIcon && <IconNerux name={endIcon} />
-          )}
-        </InputGroup.Suffix>
-      </InputGroup>
+        >
+          <InputGroup.Prefix>
+            {startIcon && <IconNerux name={startIcon} size="sm" />}
+          </InputGroup.Prefix>
+          <InputGroup.Input
+            placeholder={props.placeholder}
+            className={cn(
+              "text-(--color-primary-text-default)",
+              hasError
+                ? "text-(--color-error)"
+                : "text-(--color-primary-text-default)",
+            )}
+          />
+          <InputGroup.Suffix>
+            {type === "password" ? (
+              <Button
+                isIconOnly
+                variant="ghost"
+                size="md"
+                aria-label={isVisible ? "Hide password" : "Show password"}
+                onPress={() => setIsVisible(!isVisible)}
+              >
+                {isVisible ? (
+                  <IconNerux name="IconEyeClosed" />
+                ) : (
+                  <IconNerux name="IconEye" />
+                )}
+              </Button>
+            ) : (
+              endIcon && <IconNerux name={endIcon} />
+            )}
+          </InputGroup.Suffix>
+        </InputGroup>
 
-      {hasError ? (
-        <FieldError>
-          <div className="flex items-center gap-1">
-            <IconNerux name="IconAlertTriangle" size="sm" color="error" />
-            <TextNerux variant="error">{error}</TextNerux>
-          </div>
-        </FieldError>
-      ) : null}
-    </TextField>
+        {hasError ? (
+          <FieldError>
+            <div className="flex items-center gap-1">
+              <IconNerux name="IconAlertTriangle" size="sm" color="error" />
+              <TextNerux variant="error">{error}</TextNerux>
+            </div>
+          </FieldError>
+        ) : null}
+      </TextField>
+    </div>
   );
 }
