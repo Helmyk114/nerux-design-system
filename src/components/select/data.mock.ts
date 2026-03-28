97e0c04abe576626c424c97213dc0cd78a20087c
Sergio Esteban <sergio.calvo@udea.edu.co>
2026-03-28 16:17:47 -0500

    feat: agregar componente Select con StoryBook

---
+++ src/components/select/data.mock.ts
@@ -0,0 +1,26 @@
+export const states = [
+  {
+    id: "california",
+    name: "California",
+  },
+  {
+    id: "texas",
+    name: "Texas",
+  },
+  {
+    id: "florida",
+    name: "Florida",
+  },
+  {
+    id: "new-york",
+    name: "New York",
+  },
+  {
+    id: "illinois",
+    name: "Illinois",
+  },
+  {
+    id: "pennsylvania",
+    name: "Pennsylvania",
+  },
+];
